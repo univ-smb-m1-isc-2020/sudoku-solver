ad95cb0d7028e64971bbe4bffefcf150f55fb976
FayxChance <fayxchancetom@outlook.com>
2021-01-04 19:50:34 +0100

    Default constructor

--- src/main/java/Sudoku.java
+++ src/main/java/Sudoku.java
@@ -5,6 +5,20 @@ public class Sudoku {
     public static int COLUMN;
     private final ArrayList<Case> _grid;
 
+    public Sudoku() {
+        this(new int[][]{
+                {8, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 3, 6, 0, 0, 0, 0, 0},
+                {0, 7, 0, 0, 9, 0, 2, 0, 0},
+                {0, 5, 0, 0, 0, 7, 0, 0, 0},
+                {0, 0, 0, 0, 4, 5, 7, 0, 0},
+                {0, 0, 0, 1, 0, 0, 0, 3, 0},
+                {0, 0, 1, 0, 0, 0, 0, 6, 8},
+                {0, 0, 8, 5, 0, 0, 0, 1, 0},
+                {0, 9, 0, 0, 0, 0, 4, 0, 0}
+        });
+    }
+
     public Sudoku(int[][] map) {
         _grid = new ArrayList<>();
         for (int column = 0; column < map.length; column++) {
