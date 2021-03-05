0a676f7c7cb7a85f41a976815ef94b88d3dba97f
graveaccentquote <34614915+graveaccentquote@users.noreply.github.com>
2021-03-05 19:28:46 +0100

    updated main

--- src/main/java/sudoku/solver/SudokuSolver.java
+++ src/main/java/sudoku/solver/SudokuSolver.java
@@ -4,6 +4,9 @@ import java.util.logging.Logger;
 
 public class SudokuSolver {
 
+    private SudokuGame sudokuGame;
+    private SudokuGame solvedSudokuGame;
+
     public static void main(String[] argv)
     {
         Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
@@ -23,13 +26,41 @@ public class SudokuSolver {
         SudokuGame game = new SudokuGame(board);
         SudokuGame game2 = new SudokuGame("./boards/board1.json");
 
-        logger.log(Level.INFO, "First board : \n{0}",  game);
-        logger.log(Level.INFO, "First board : \n{0}",  game2);
+        SudokuSolver solver = new SudokuSolver(game);
+        SudokuSolver solver2 = new SudokuSolver((game2));
+
+        solver.solveGame();
+        solver2.solveGame();
+
+        logger.log(Level.INFO, "First board : \n{0}",  solver);
+        logger.log(Level.INFO, "First board : \n{0}",  solver2);
+
+    }
+
+    public SudokuSolver(SudokuGame sudokuGame)
+    {
+        this.sudokuGame = sudokuGame;
+        this.solvedSudokuGame = new SudokuGame(sudokuGame);
+
+        this.solvedSudokuGame.fillTile(2, 2, Value.NINE);
+    }
+
+    @Override
+    public String toString()
+    {
+        StringBuilder acc = new StringBuilder();
+
+        acc.append("original board :\n");
+        acc.append(this.sudokuGame);
+        acc.append("\n\n solved board :\n");
+        acc.append(this.solvedSudokuGame);
+
+        return acc.toString();
     }
 
 
-    private void solveSudoku(SudokuGame game)
+    private void solveGame()
     {
-        //TODO
+        solvedSudokuGame.getEmptyTiles();
     }
 }
