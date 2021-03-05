890db38d167d9e18055b0164f62cd3c97dc46f41
graveaccentquote <34614915+graveaccentquote@users.noreply.github.com>
2021-03-05 18:26:58 +0100

    refactor : use of loggers instead of system.out

--- src/main/java/sudoku/solver/SudokuSolver.java
+++ src/main/java/sudoku/solver/SudokuSolver.java
@@ -1,14 +1,18 @@
 package sudoku.solver;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class SudokuSolver {
 
     public static void main(String[] argv)
     {
+        Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+        logger.setLevel(Level.ALL);
+
         SudokuGame game = new SudokuGame();
         SudokuGame game2 = new SudokuGame("./boards/board1.json");
 
-        System.out.println(game.toString());
-
-        System.out.println(game2.toString());
+        logger.info("First board : \n" + game.toString());
+        logger.info("Second board : \n" + game2.toString());
     }
 }
