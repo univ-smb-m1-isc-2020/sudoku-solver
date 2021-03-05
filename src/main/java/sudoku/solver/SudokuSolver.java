38c0fa1b8ac9ec4eee1cf0e526b5f5a4a84a4488
graveaccentquote <34614915+graveaccentquote@users.noreply.github.com>
2021-03-05 18:41:56 +0100

    refactor : conditional string concatenation

--- src/main/java/sudoku/solver/SudokuSolver.java
+++ src/main/java/sudoku/solver/SudokuSolver.java
@@ -7,12 +7,23 @@ public class SudokuSolver {
     public static void main(String[] argv)
     {
         Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-        logger.setLevel(Level.ALL);
 
-        SudokuGame game = new SudokuGame();
+        int[][] board = {
+                { 8, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 3, 6, 0, 0, 0, 0, 0 },
+                { 0, 7, 0, 0, 9, 0, 2, 0, 0 },
+                { 0, 5, 0, 0, 0, 7, 0, 0, 0 },
+                { 0, 0, 0, 0, 4, 5, 7, 0, 0 },
+                { 0, 0, 0, 1, 0, 0, 0, 3, 0 },
+                { 0, 0, 1, 0, 0, 0, 0, 6, 8 },
+                { 0, 0, 8, 5, 0, 0, 0, 1, 0 },
+                { 0, 9, 0, 0, 0, 0, 4, 0, 0 }
+        };
+
+        SudokuGame game = new SudokuGame(board);
         SudokuGame game2 = new SudokuGame("./boards/board1.json");
 
-        logger.info("First board : \n" + game.toString());
-        logger.info("Second board : \n" + game2.toString());
+        logger.log(Level.INFO, "First board : \n{0}",  game.toString());
+        logger.log(Level.INFO, "First board : \n{0}",  game2.toString());
     }
 }
