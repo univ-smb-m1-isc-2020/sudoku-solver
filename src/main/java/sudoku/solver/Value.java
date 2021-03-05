ba6e14e3982e3521eb11d6d48b67c7e53002eabc
graveaccentquote <34614915+graveaccentquote@users.noreply.github.com>
2021-03-05 09:32:50 +0100

    refactor : modified default toString()

--- src/main/java/sudoku/solver/Value.java
+++ src/main/java/sudoku/solver/Value.java
@@ -19,27 +19,33 @@ public enum Value {
         value = newValue;
     }
 
-    public int getValue() { return value; }
+    public int getValue() {
+        return value;
+    }
 
 
-    public static Value[][] convertIntArray(int[][] intValues)
-    {
+    public static Value[][] convertIntArray(int[][] intValues) {
         int rows = intValues.length;
         int columns = intValues[0].length;
 
-        assert(rows == columns);
+        assert (rows == columns);
 
 
         Value[][] result = new Value[rows][columns];
 
-        for (int i=0; i<rows; ++i)
-        {
-            for (int j=0; j<columns; ++j)
-            {
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < columns; ++j) {
                 result[i][j] = Value.values()[intValues[i][j]];
             }
         }
 
         return result;
+    }
+
+    public String toString() {
+        if (this.value != -1)
+            return Integer.toString(this.value);
+        else
+            return "_";
     }
 }
