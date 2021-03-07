eec589babe6b639311828a5cc9062a1172e58bc8
Kubasik <tomkubasik.pro@gmail.com>
2021-03-08 00:06:20 +0100

    tant pis

--- src/main/java/sodoku/Board.java
+++ src/main/java/sodoku/Board.java
@@ -1,30 +1,26 @@
 package sodoku;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Board {
-    private int[][] board;
+    private int[][] boardArray;
     private int boardLength;
 
     public Board(int[][] board) {
-        this.board = board;
+        this.boardArray = board;
         this.boardLength = board.length;
     }
 
-    public int[][] getBoard() {
-        return board;
-    }
-
-    public void setBoard(int[][] board) {
-        this.board = board;
-    }
 
     @Override
     public String toString() {
 
         String result = "";
         for (int i = 0; i < boardLength; i++) {
-            result += ("{" + Arrays.toString(board[i]) + "} \n");
+            result += ("{" + Arrays.toString(boardArray[i]) + "} \n");
         }
 
         return result;
@@ -32,44 +28,36 @@ public class Board {
 
 
     public Board SearchSoluce(){
+        int[][] boardSoluce = copyArray();
+        boolean good = false;
 
-        return this;
+        while (!good){
+            //System.out.println(good);
+            boardSoluce = getSoluce(copyArray());
+            good = verifBoard(boardSoluce);
+
+        }
+
+        return new Board(boardSoluce);
     }
 
-    public boolean verifBoard(){
-
+    public boolean verifBoard(int[][] _boardArray){
+        //System.out.println("verifBoard");
+        boolean result = true;
         for (int i = 0; i < boardLength; i++) {
-            verifRow(i);
-            verifColumn(i);
+            if(result == true){
+                    result = verifRow(i, _boardArray);
+            }
+
+           if(result == true){
+                result = verifColumn(i, _boardArray);
+           }
         }
 
         for (int i = 0; i < boardLength / 3; i++) {
-            verifBlock(i);
-        }
-        return true;
-    }
-
-    public boolean verifRow(int indexRow){
-
-        boolean result = true;
-
-        for (int i = 0; i < boardLength; i++) {
-            for (int j = i + 1 ; j < boardLength; j++) {
-                if (board[indexRow][i] == board[indexRow][j]) {
-                    result = false;
-                }
-            }
-        }
-        return result;
-    }
-
-    public boolean verifColumn(int indexColumn){
-        boolean result = true;
-
-        for (int i = 0; i < boardLength; i++) {
-            for (int j = i + 1 ; j < boardLength; j++) {
-                if (board[i][indexColumn] == board[j][indexColumn]) {
-                    result = false;
+            for (int j = 0; i < boardLength / 3; j++) {
+                if(result == true){
+                    result = verifBlock(i,j, _boardArray);
                 }
             }
         }
@@ -77,9 +65,103 @@ public class Board {
         return result;
     }
 
-    public boolean verifBlock(int indexBlock){
-        return true;
+    public boolean verifRow(int indexRow, int[][] _boardArray){
+        //System.out.println("verifRow");
+
+        boolean result = true;
+
+        for (int i = 0; i < boardLength; i++) {
+            for (int j = i + 1 ; j < boardLength; j++) {
+                if (_boardArray[indexRow][i] == _boardArray[indexRow][j]) {
+                    result = false;
+                }
+            }
+        }
+        return result;
     }
+
+    public boolean verifColumn(int indexColumn, int[][] _boardArray){
+        System.out.print(indexColumn);
+        boolean resultColumn = true;
+
+        for (int i = 0; i < boardLength; i++) {
+            for (int j = i + 1 ; j < boardLength; j++) {
+                if (_boardArray[i][indexColumn] == _boardArray[j][indexColumn]) {
+                    resultColumn = false;
+                }
+            }
+        }
+
+        return resultColumn;
+    }
+
+    public boolean verifBlock(int indexRow, int indexColumn, int[][] _boardArray){
+        System.out.println("verifBlock");
+
+        boolean result = true;
+        int firstIndexRow = 3 * indexRow;
+        int firstIndexCollumn = 3 * indexColumn;
+
+        ArrayList<Integer> blockList = new ArrayList<Integer>();
+
+        for (int i = firstIndexRow; i < firstIndexRow + 3; i++) {
+            for (int j = firstIndexCollumn ; j < firstIndexCollumn + 3; j++) {
+                blockList.add(_boardArray[i][j]);
+            }
+        }
+
+        for (int i = 0; i < blockList.size(); i++) {
+            for (int j = i + 1 ; j < blockList.size(); j++) {
+                if (blockList.get(i) == blockList.get(j)) {
+                    result = false;
+                }
+            }
+        }
+
+
+        return result;
+    }
+
+
+    public int[][] getSoluce(int[][] _boardArray){
+        System.out.print("s");
+        for (int i = 0; i < boardLength; i++) {
+            for (int j = 0 ; j < boardLength; j++) {
+                if (_boardArray[i][j] == 0) {
+                    Random r = new Random();
+                    _boardArray[i][j] =  r.nextInt((9 - 1) + 1) + 1;
+                }
+            }
+        }
+        System.out.println(ArraytoString(_boardArray));
+        return _boardArray;
+
+    }
+
+    public String ArraytoString(int[][] _boardArray) {
+
+        String result = "";
+        for (int i = 0; i < boardLength; i++) {
+            result += ("{" + Arrays.toString(_boardArray[i]) + "} \n");
+        }
+
+        return result;
+    }
+
+
+    public int[][] copyArray(){
+        int [][] myInt = new int[boardArray.length][];
+        for(int i = 0; i < boardArray.length; i++)
+        {
+            int[] aMatrix = boardArray[i];
+            int   aLength = aMatrix.length;
+            myInt[i] = new int[aLength];
+            System.arraycopy(aMatrix, 0, myInt[i], 0, aLength);
+        }
+
+        return myInt;
+    }
+
 
 
 
--- src/test/java/sodoku/BoardTest.java
+++ src/test/java/sodoku/BoardTest.java
@@ -6,7 +6,7 @@ public class BoardTest {
 
 
     @Test
-    void name() {
+    void textColumn() throws Exception{
 
     }
 }
